00d6cad0136fcc80f6d1556dba17582839d02b9e
David Johnston <davidoj@fastmail.com.au>
2024-07-08 01:04:19 +0000

    cache fixes

--- src/cupbearer/scripts/cache_activations.py
+++ src/cupbearer/scripts/cache_activations.py
@@ -6,21 +6,31 @@ from cupbearer.detectors.activations import get_last_token_activation_function_f
 import torch
 
 datasets = [
-    "capitals", "hemisphere", "population", "sciq", "sentiment", "nli",
-    "authors", "addition", "subtraction", "multiplication", "modularaddition", "squaring"
+    "capitals",
+    "hemisphere",
+    "population",
+    "sciq",
+    "sentiment",
+    "nli",
+    "authors",
+    "addition",
+    "subtraction",
+    "multiplication",
+    "modularaddition",
+    "squaring"
 ]
 
-def build_cache(dataset, layers, batch_size):
+def build_cache(dataset, layers, batch_size, first_layer, last_layer, interval):
     print(f"Processing dataset: {dataset}")
 
     if dataset in ['sciq', 'sentiment']:
         batch_size //= 4
 
-    task = tasks.quirky_lm(include_untrusted=True, mixture=True, standardize_template=True, dataset=dataset, easy_quantile=1, hard_quantile=0)
+    task = tasks.quirky_lm(include_untrusted=True, mixture=True, standardize_template=True, random_names=True, dataset=dataset, easy_quantile=0.25, hard_quantile=0.75)
     
     activation_processing_function = get_last_token_activation_function_for_task(task)
     
-    cache_path = f"cache/{dataset}-{task.model.hf_model.config.name_or_path.split('/')[-1]}.pt"
+    cache_path = f"cache/{dataset}-{task.model.hf_model.config.name_or_path.split('/')[-1]}-{first_layer}-{last_layer}-{interval}.pt"
     
     # if the cache file exists, skip
     if Path(cache_path).exists():
@@ -46,13 +56,14 @@ def build_cache(dataset, layers, batch_size):
     torch.cuda.empty_cache()
 
 def main(first_layer, last_layer, batch_size):
-    layers = list(range(first_layer, last_layer + 1))
+    interval = (last_layer - first_layer)//8
+    layers = list(range(first_layer, last_layer + 1, interval))
 
     layer_list = [f"hf_model.model.layers.{layer}.input_layernorm.input" for layer in layers]
     layer_list += [f"hf_model.model.layers.{layer}.self_attn.output" for layer in layers]
 
     for dataset in datasets:
-        build_cache(dataset, layer_list, batch_size)
+        build_cache(dataset, layer_list, batch_size, first_layer, last_layer, interval)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run CacheBuilder for quirky datasets")
